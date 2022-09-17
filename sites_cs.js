ae72151eb8c1232351c72211c7216f4068fc1f0b
dhjw <deanhjw@gmail.com>
2022-09-16 21:50:38 -0700

    Add site

--- sites_cs.js
+++ sites_cs.js
@@ -3,7 +3,9 @@ console.log('np2a: sites_cs.js loaded')
 var rx = [
 	'^(https://www.bloomberg.com/news/[^?#]*)',
 	'^(https://www.forbes.com/sites/[^?#]*)',
+	'^(https://foreignpolicy.com/\\d+/\\d+/\\d+/.*/)',
 	'^https://www.heraldsun.com.au/subscribe/news/.*&dest=([^&]*)',
+	"(https://www.nationalgeographic.com/[^/]*/article/[^?#]*)",
 	'^(https://www.nytimes.com/.*\.html)',
 	'^https://www.theaustralian.com.au/subscribe/news/.*&dest=([^&]*)',
 	'^(https://www.theepochtimes.com/.*\.html)',
@@ -11,7 +13,6 @@ var rx = [
 	'^(https://www.washingtonpost.com/.*?/\\d+/\\d+/\\d+/.*?/)',
 	'^(https://www.washingtontimes.com/news/\\d+/\\w{3}/\\d+/.*?/)',
 	'^(https://www.wsj.com/articles/[^?#]*)',
-	'^(https://foreignpolicy.com/\\d+/\\d+/\\d+/.*/)'
 
 ], m
 
