5e171e63657d9e789fce3a922a35de46e586b340
dhjw <deanhjw@gmail.com>
2022-09-26 02:04:15 -0700

    Improve regexes

--- sites_cs.js
+++ sites_cs.js
@@ -3,15 +3,15 @@ console.log('np2a: sites_cs.js loaded')
 var rx = [
 	'^(https://www.bloomberg.com/news/[^?#]*)',
 	'^(https://www.forbes.com/sites/[^?#]*)',
-	'^(https://foreignpolicy.com/\\d+/\\d+/\\d+/.*/)',
+	'^(https://foreignpolicy.com/\\d{4}/\\d{2}/\\d{2}/.*?/)',
 	'^https://www.heraldsun.com.au/subscribe/news/.*&dest=([^&]*)',
 	'^(https://www.nationalgeographic.com/[^/]*/article/[^?#]*)',
 	'^(https://www.nytimes.com/.*\.html)',
-	'^(https://www.telegraph.co.uk/[^/]*/\\d{4}/\\d{2}/\\d{2}/.*/)',
+	'^(https://www.telegraph.co.uk/[^/]*?/\\d{4}/\\d{2}/\\d{2}/.*?/)',
 	'^https://www.theaustralian.com.au/subscribe/news/.*&dest=([^&]*)',
 	'^(https://www.theepochtimes.com/.*\.html)',
 	'^(https://www.thetimes.co.uk/article/[^?#]*)',
-	'^(https://www.washingtonpost.com/.*?/\\d+/\\d+/\\d+/.*?/)',
+	'^(https://www.washingtonpost.com/[^/]*?/\\d{4}/\\d{2}/\\d{2}/.*?/)',
 	'^(https://www.washingtontimes.com/news/\\d+/\\w{3}/\\d+/.*?/)',
 	'^(https://www.wsj.com/articles/[^?#]*)',
 
